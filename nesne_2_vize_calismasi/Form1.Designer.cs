1e80461923cf8d1d08c8f3f1cf14b4cad4a86a99
Yusuf Kasım <https://github.com/ksyusuf>
2022-05-06 21:09:13 +0300

    Initial commit

---
+++ nesne_2_vize_calismasi/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace nesne_2_vize_calismasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(60, 47);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(186, 55);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "label10";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(60, 229);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(186, 55);
+            this.label11.TabIndex = 7;
+            this.label11.Text = "label11";
+            this.label11.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(584, 161);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DÜNYANIN EN MÜTHİŞ YAŞ HESAPLAMA PROGRAMI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ nesne_2_vize_calismasi/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nesne_2_vize_calismasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        int vize, final;
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+        double ort;
+        string hbn;
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime bugunTarihi = DateTime.Now;
+            DateTime dogum1 = new DateTime(1999, 6, 4);
+            DateTime dogum2 = new DateTime(2001, 11, 11);
+
+            TimeSpan ts1 = bugunTarihi - dogum1;
+            TimeSpan ts2 = bugunTarihi - dogum2;
+            label10.Text = "" + (ts1.TotalMilliseconds/(1000*60*60*24*365.25)).ToString();
+            label11.Text = "" + (ts2.TotalMilliseconds / (1000 * 60 * 60 * 24 * 365.25)).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+               
+        }
+    }
+}
