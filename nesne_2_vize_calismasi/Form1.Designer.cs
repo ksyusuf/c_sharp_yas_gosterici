418ca9d5b9878d7109fcbb84bc46bc156104a005
Yusuf Kasım <https://github.com/ksyusuf>
2022-05-06 21:49:03 +0300

    ilk çalışma

--- nesne_2_vize_calismasi/Form1.Designer.cs
+++ nesne_2_vize_calismasi/Form1.Designer.cs
@@ -32,7 +32,19 @@ namespace nesne_2_vize_calismasi
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
@@ -44,45 +56,162 @@ namespace nesne_2_vize_calismasi
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label10.Location = new System.Drawing.Point(60, 47);
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(155, 36);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(186, 55);
+            this.label10.Size = new System.Drawing.Size(125, 37);
             this.label10.TabIndex = 6;
             this.label10.Text = "label10";
             this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label11
+            // label2
             // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label11.Location = new System.Drawing.Point(60, 229);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(186, 55);
-            this.label11.TabIndex = 7;
-            this.label11.Text = "label11";
-            this.label11.Visible = false;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(23, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 37);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Yusuf -";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(37, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 37);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ayşe -";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(155, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 37);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(560, 389);
+            this.tabControl1.TabIndex = 13;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(552, 363);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Mevcut Yaşlar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(552, 363);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sonraki Yaşa Kalan";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(155, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 37);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(37, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 37);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Ayşe -";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(23, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 37);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Yusuf -";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(155, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 37);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "label7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(584, 161);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
+            this.ClientSize = new System.Drawing.Size(584, 413);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DÜNYANIN EN MÜTHİŞ YAŞ HESAPLAMA PROGRAMI";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- nesne_2_vize_calismasi/Form1.cs
+++ nesne_2_vize_calismasi/Form1.cs
@@ -19,35 +19,55 @@ namespace nesne_2_vize_calismasi
 
         private void label2_Click(object sender, EventArgs e)
         {
-
         }
-        int vize, final;
 
         private void groupBox2_Enter(object sender, EventArgs e)
         {
-
         }
-        double ort;
-        string hbn;
         private void button2_Click(object sender, EventArgs e)
         {
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        void yil_yas_hesaplayici(int gun, int ay, int yil, Label label_adi)
         {
             DateTime bugunTarihi = DateTime.Now;
-            DateTime dogum1 = new DateTime(1999, 6, 4);
-            DateTime dogum2 = new DateTime(2001, 11, 11);
+            DateTime dogum1 = new DateTime(yil, ay, gun);
 
             TimeSpan ts1 = bugunTarihi - dogum1;
-            TimeSpan ts2 = bugunTarihi - dogum2;
-            label10.Text = "" + (ts1.TotalMilliseconds/(1000*60*60*24*365.25)).ToString();
-            label11.Text = "" + (ts2.TotalMilliseconds / (1000 * 60 * 60 * 24 * 365.25)).ToString();
+            label_adi.Text = "" + (ts1.TotalMilliseconds / (1000 * 60 * 60 * 24 * 365.25)).ToString();
+        }
+        void yil_yas_dogum_gunu_kalan(int gun, int ay, int yil, Label label_adi)
+        {
+            DateTime gelecek_yil;
+            if (DateTime.Now > new DateTime(DateTime.Now.Year, ay, gun))
+            {
+                // eğer mevcut yıl içerisinde o doğmu günü geçmişse
+                // sonraki yıla göre kalan hesabı yapıyor işte.
+                gelecek_yil = new DateTime(DateTime.Now.Year+1, ay, gun);
+            }
+            else
+            {
+                gelecek_yil = new DateTime(DateTime.Now.Year, ay, gun);
+            }
+            DateTime dogum1 = DateTime.Now;
+            TimeSpan kalan_yil = gelecek_yil - dogum1;
+            label_adi.Text = "" + (kalan_yil.TotalMilliseconds / (1000 * 60 * 60 * 24 * 365.25)).ToString();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //fonksiyon burada çalışacak
+            yil_yas_hesaplayici(4, 6, 1999, label10); //ysf
+            yil_yas_hesaplayici(16, 1, 1999, label1); //elife
+            yil_yas_dogum_gunu_kalan(4, 6, 1999, label7); //ysf
+            yil_yas_dogum_gunu_kalan(16, 1, 1999, label4); //elife
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-               
         }
     }
 }
